06df7a0d10407c6b085732cb7de99454bf7ad042
Charlie Bini <5003326+cbini@users.noreply.github.com>
2021-12-16 16:25:02 -0600

    initial commit

---
+++ njss/extract-students.sh
@@ -0,0 +1,2 @@
+cd ~/projects/datagun
+./bin/qgtunnel ~/.pyenv/versions/datagun/bin/python ./datagun/extract.py --config ./datagun/config/njss.json
---
+++ njss/isr-splitter.py
@@ -0,0 +1,70 @@
+import os
+import json
+import pathlib
+import re
+
+from dotenv import load_dotenv
+from PyPDF2 import PdfFileWriter, PdfFileReader
+
+load_dotenv()
+
+input_dir = os.getenv("INPUT_DIR")
+student_file = os.getenv("STUDENT_FILE")
+
+regex_pattern = "LocalStudentIdentification:(?P<LocalStudentIdentification>\d+)"
+
+with open(student_file) as f:
+    student_lookup = json.load(f)
+
+input_path = pathlib.Path(input_dir)
+processed_path = input_path / "processed"
+
+input_pdfs = [f for f in input_path.glob("*.pdf")]
+
+if not processed_path.exists():
+    processed_path.mkdir(parents=True)
+
+for pdf in sorted(input_pdfs):
+    print(pdf.stem)
+    pdf_in = PdfFileReader(open(pdf, "rb"))
+
+    for p in range(pdf_in.numPages - 1):
+        if p % 2 == 0:
+            page = pdf_in.getPage(p)
+            page_text = page.extractText()
+
+            regex_match = re.search(regex_pattern, page_text)
+            regex_group = regex_match.groupdict()
+            student_number = int(regex_group.get("LocalStudentIdentification"))
+
+            student_match = next(
+                iter(
+                    [s for s in student_lookup if s["student_number"] == student_number]
+                ),
+                {
+                    "student_number": 999999,
+                    "grade_level": 99,
+                    "abbreviation": "UNMATCHED",
+                },
+            )
+
+            student_file_name = f"{student_match['abbreviation']}_{student_match['student_number']}_{pdf.name}"
+            student_file_path = (
+                input_path / "out" / student_match["abbreviation"] / student_file_name
+            )
+
+            if not student_file_path.exists():
+                if not student_file_path.parent.exists():
+                    print(f"\tCreating {student_file_path.parent}...")
+                    student_file_path.parent.mkdir(parents=True)
+
+                pdf_out = PdfFileWriter()
+                pdf_out.addPage(pdf_in.getPage(p))
+                pdf_out.addPage(pdf_in.getPage(p + 1))
+
+                print(f"\t{student_file_name}")
+                with student_file_path.open("wb+") as f:
+                    pdf_out.write(f)
+
+    pdf.rename(pdf.parent / "processed" / pdf.name)
+    print(f"\tMoved to ./{'processed' / pdf.name}")
